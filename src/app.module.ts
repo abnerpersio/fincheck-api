5ffc6725f4c472cf472b19ad37cc40ec4ae5c427
Abner Persio <binhopersio@gmail.com>
2024-12-03 07:54:14 -0300

    feat: app module with guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { DatabaseModule } from './infra/database/database.module';
+import { AuthGuard } from './modules/auth/auth.guard';
+import { AuthModule } from './modules/auth/auth.module';
+import { TransactionCategoriesModule } from './modules/transaction-categories/transaction-categories.module';
 import { UsersModule } from './modules/users/users.module';
+import { BankAccountsModule } from './modules/bank-accounts/bank-accounts.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    UsersModule,
+    AuthModule,
+    TransactionCategoriesModule,
+    BankAccountsModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Env } from './infra/config/env';
+import { env } from './infra/config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(Env.get('PORT'));
+  await app.listen(env.port);
 }
 
 bootstrap();
